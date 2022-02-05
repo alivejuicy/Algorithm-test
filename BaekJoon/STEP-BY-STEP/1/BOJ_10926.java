9d370e8ef2b0a410fd01695ddaf34895a8c820c5
dhkwak7791 <kkddhh1998@gmail.com>
2022-02-05 10:19:02 +0900

    Clear

---
+++ BaekJoon/STEP-BY-STEP/1/BOJ_10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.nextLine();
+
+        System.out.println(s + "??!");
+    }
+}
