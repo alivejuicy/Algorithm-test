dab5b874d54359725886a185308afb4c54efda5a
Kwak donghyun <80264449+alivejuicy@users.noreply.github.com>
2023-09-13 00:14:01 +0900

    [Silver II] Title: 부분수열의 합, Time: 136 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,26 +1,29 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int n, s;
-    static int[] arr;
     static int counter = 0;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        s = sc.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
         arr = new int[n];
-
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int temp = 0;
-        solution(0, 0);
-        counter = s == 0 ? counter - 1 : counter; //공집합 제거 요소
+        int tmp = 0;
+        recursion(0, tmp);
+        if (s == 0) counter -= 1;
         System.out.println(counter);
     }
 
-    private static void solution(int start, int temp) {
+    private static void recursion(int start, int temp) {
         if (start == n) {
             if (temp == s) {
                 counter++;
@@ -28,8 +31,8 @@ public class Main {
             return;
         }
         //현재 원소를 선택한 경우
-        solution(start + 1, temp + arr[start]);
+        recursion(start + 1, temp + arr[start]);
         //현재 원소를 선택하지 않은 경우
-        solution(start + 1, temp);
+        recursion(start + 1, temp);
     }
 }
