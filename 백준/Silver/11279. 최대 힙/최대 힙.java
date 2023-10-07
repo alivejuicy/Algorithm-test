f5ffc27d2f7e1058a83fedd0f391a3551762dfe2
Kwak donghyun <80264449+alivejuicy@users.noreply.github.com>
2023-10-07 12:29:51 +0900

    [Silver II] Title: 최대 힙, Time: 344 ms, Memory: 26320 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if (a == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(a);
+            }
+        }
+        System.out.println(sb);
+    }
+}
